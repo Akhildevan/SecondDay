7959358c33d8d845eb8a6098341d33bec91f2541
Akhildevan <akhildevan1997@gmail.com>
2021-07-25 16:39:18 +0530

    create loop

--- src/com/pro/Car.java
+++ src/com/pro/Car.java
@@ -1,5 +1,18 @@
 package com.pro;
 
 public class Car {
+	private void lamborgini() {
+	System.out.println("lamborgini goast");
+	}
+	private void benz() {
+		System.out.println("benz classic");
+	}
 
+	public static void main(String[] args) {
+		Car c = new Car ();
+		c.benz();
+		c.lamborgini();
+	}
+	
+	
 }
